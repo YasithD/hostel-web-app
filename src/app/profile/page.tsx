8fb72877992b6a872618d6528ca44bfebe0c6293
YasithD <yasith.18@cse.mrt.ac.lk>
2026-01-07 13:43:22 +1000

    Revamp Profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -7,7 +7,8 @@ import { User } from "@/db/schema";
 import axiosInstance from "@/utils/axios";
 import { useAuth, useUser } from "@clerk/nextjs";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Edit } from "lucide-react";
+import { ArrowLeftIcon, Edit, LockIcon } from "lucide-react";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
@@ -68,22 +69,30 @@ export default function Profile() {
         lastName: data.lastName,
       });
     }
+
+    setIsEditing({ firstName: false, lastName: false });
   };
 
   return (
-    <div className="h-[calc(100vh-6.5rem)] sm:pt-10">
-      <div className="flex flex-col mx-auto w-[400px] p-10 sm:shadow-[0_0_16px_4px_rgba(0,0,0,0.1)] rounded-lg">
-        <p className="text-3xl font-semibold text-primary mb-10">User Profile</p>
+    <div className="h-[calc(100vh-6.5rem)] sm:pt-10 bg-primary-gradient relative">
+      {/* Home Buttom */}
+      <Link href="/dashboard" className="flex items-center gap-2 py-2 px-4 mx-auto bg-secondary rounded-lg w-fit">
+        <ArrowLeftIcon size={16} />
+        <p className="text-sm font-medium text-foreground">Dashboard</p>
+      </Link>
+
+      {/* Profile Form */}
+      <div className="mt-5 flex flex-col mx-auto w-[400px] p-10 bg-white shadow-[0_4px_24px_8px_rgba(0,0,0,0.12)] rounded-lg">
+        <div className="flex flex-col gap-4 mb-10 justify-center items-center">
+          <p className="text-3xl font-semibold text-primary">User Profile</p>
+          <p className="text-sm text-muted">Manage your personal information.</p>
+          <div className="w-20 h-1 bg-secondary rounded-full" />
+        </div>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="flex flex-col gap-2">
               <div className="flex gap-2 items-center">
-                <p className="text-sm font-medium text-foreground-muted">First Name</p>
-                <Edit
-                  className="cursor-pointer"
-                  size={16}
-                  onClick={() => setIsEditing({ ...isEditing, firstName: true })}
-                />
+                <p className="text-sm font-semibold text-foreground-muted">First Name</p>
               </div>
               <FormField
                 control={form.control}
@@ -94,7 +103,14 @@ export default function Profile() {
                       {isEditing.firstName ? (
                         <Input className="text-sm" {...field} />
                       ) : (
-                        <p className="text-sm font-medium text-foreground">{field.value}</p>
+                        <div className="flex justify-between items-center border-b border-border pb-1">
+                          <p className="text-sm font-semibold text-foreground">{field.value}</p>
+                          <Edit
+                            className="cursor-pointer"
+                            size={16}
+                            onClick={() => setIsEditing({ ...isEditing, firstName: true })}
+                          />
+                        </div>
                       )}
                     </FormControl>
                     <FormMessage className="absolute my-0 bottom-0 translate-y-full pt-1" />
@@ -104,12 +120,7 @@ export default function Profile() {
             </div>
             <div className="flex flex-col gap-2">
               <div className="flex gap-2 items-center">
-                <p className="text-sm font-medium text-foreground-muted">Last Name</p>
-                <Edit
-                  className="cursor-pointer"
-                  size={16}
-                  onClick={() => setIsEditing({ ...isEditing, lastName: true })}
-                />
+                <p className="text-sm font-semibold text-foreground-muted">Last Name</p>
               </div>
               <FormField
                 control={form.control}
@@ -120,7 +131,14 @@ export default function Profile() {
                       {isEditing.lastName ? (
                         <Input className="text-sm" {...field} />
                       ) : (
-                        <p className="text-sm font-medium text-foreground">{field.value}</p>
+                        <div className="flex justify-between items-center border-b border-border pb-1">
+                          <p className="text-sm font-medium text-foreground">{field.value}</p>
+                          <Edit
+                            className="cursor-pointer"
+                            size={16}
+                            onClick={() => setIsEditing({ ...isEditing, lastName: true })}
+                          />
+                        </div>
                       )}
                     </FormControl>
                     <FormMessage className="absolute my-0 bottom-0 translate-y-full pt-1" />
@@ -129,15 +147,20 @@ export default function Profile() {
               />
             </div>
             <div className="flex gap-8">
-              <div className="flex flex-col gap-2">
-                <p className="text-sm font-medium text-foreground-muted">Email</p>
-                <p className="text-sm font-medium text-foreground">{user?.emailAddresses[0].emailAddress ?? "N/A"}</p>
+              <div className="flex flex-col w-full gap-2">
+                <p className="text-sm font-semibold text-foreground-muted">Email</p>
+                <div className="flex gap-2 items-center border-b border-border pb-1">
+                  <LockIcon size={16} color="#4d4d4d" />
+                  <p className="w-full text-sm font-medium text-muted">
+                    {user?.emailAddresses[0].emailAddress ?? "N/A"}
+                  </p>
+                </div>
               </div>
             </div>
             <div className="flex gap-8">
               <div className="flex flex-col gap-2">
-                <p className="text-sm font-medium text-foreground-muted">User Type</p>
-                <p className="text-sm font-medium text-foreground">
+                <p className="text-sm font-semibold text-foreground-muted">User Type</p>
+                <p className="text-xs w-fit text-foreground py-1 px-2 rounded-md bg-success">
                   {(user?.publicMetadata.role as string)?.toUpperCase()}
                 </p>
               </div>
